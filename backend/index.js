c8281e5079537beab79e036e3bbcdcbaf091b6e8
Vardhan Gollapalli <vardhangollapalli87@gmail.com>
2025-06-07 11:51:30 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
-import userRouter from './routes/userRoute.js';
-import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
 import dotenv from 'dotenv';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import path from "path";
+
 
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 //app config
 const app = express();
@@ -23,25 +25,19 @@ app.use(cors());
 //db connection
 connectDB();
 
-// Serve static files from the admin's dist folder
-app.use(express.static(path.join(__dirname, '../admin/dist')));
 
 //api endpoints
 app.use('/api/food',foodRouter);
 app.use('/images',express.static("uploads"));
-app.use('/api/user',userRouter)
-app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
 
-
-
-
-// Fallback: serve index.html for any route not handled by API
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../admin/dist', 'index.html'));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
